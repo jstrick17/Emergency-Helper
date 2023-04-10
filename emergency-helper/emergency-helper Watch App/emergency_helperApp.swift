7441cac564f9778a01c80e6491aa5cc34c52659f
Calob Horton <hortocal@mail.gvsu.edu>
2023-04-10 19:30:52 -0400

    Started Xcode project...

---
+++ emergency-helper/emergency-helper Watch App/emergency_helperApp.swift
@@ -0,0 +1,17 @@
+//
+//  emergency_helperApp.swift
+//  emergency-helper Watch App
+//
+//  Created by Calob Horton on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct emergency_helper_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
