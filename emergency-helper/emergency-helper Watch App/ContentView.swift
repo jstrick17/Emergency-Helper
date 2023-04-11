24ee01962deec3626e0e4d522dc69fc8ef81ded6
Calob Horton <hortocal@mail.gvsu.edu>
2023-04-11 12:09:06 -0400

    Saving new cell view

--- emergency-helper/emergency-helper Watch App/ContentView.swift
+++ emergency-helper/emergency-helper Watch App/ContentView.swift
@@ -10,11 +10,53 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Button("Open First Aid Booklet"){}
-            Button("Add/Call Emergency Contacts"){}
-            Button("Find Nearest Hospital"){}
+            FirstAidCellView()
+            EmergencyContactsCellView()
+            NearestHospitalCellView()
         }
-        .padding()
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+    }
+}
+
+struct FirstAidCellView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            HStack(alignment: .center) {
+                Image(systemName: "text.book.closed.fill").resizable().aspectRatio(contentMode: .fit).foregroundColor(.red)
+                Spacer()
+                Text("Open First Aid Booklet")
+                Spacer()
+                Image(systemName: "arrow.right")
+            }
+        }
+    }
+}
+
+struct EmergencyContactsCellView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            HStack(alignment: .center) {
+                Image(systemName: "person.crop.circle.badge.exclamationmark").resizable().aspectRatio(contentMode: .fit).foregroundColor(.red)
+                Spacer()
+                Text("Emergency Contacts")
+                Spacer()
+                Image(systemName: "arrow.right")
+            }
+        }
+    }
+}
+
+struct NearestHospitalCellView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            HStack(alignment: .center) {
+                Image(systemName: "building.2").resizable().aspectRatio(contentMode: .fit).foregroundColor(.red)
+                Spacer()
+                Text("Find Hospital")
+                Spacer()
+                Image(systemName: "arrow.right")
+            }
+        }
     }
 }
 
